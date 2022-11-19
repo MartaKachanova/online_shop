21b33eb2d07700c606961e336973e13e952ecc11
Marta_kachanova <marta.kachanova@gmail.com>
2022-11-19 23:46:25 +0200

    Integrate React Router

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import "./assets/css/style.css";
 // import db from "./db";
 // import { useState } from "react";
 import Header from "./components/Header";
-import FavouritesList from "./components/FavouritesList";
-import Account from "./components/Account";
+import { Outlet } from "react-router-dom";
 
 function App() {
   // const [users, setUsers] = useState(db.usersDb);
   // const [products, setProducts] = useState(db.productsDb);
 
-  return <><Header /><main>
-  <div className="container"><Account/>
-  </div>
-  
-  </main></>; 
+  return (
+    <>
+      <Header />
+      <main>
+        <div className="container">
+          <Outlet/>
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default App;
- 
